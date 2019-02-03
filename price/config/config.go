a402c6f37343a426b360a69dfef39c9507d59ebc
Vadim Filatov <default87@gmail.com>
2019-02-03 15:41:49 +0100

    update vendor. Still failed to build.

--- price/config/config.go
+++ price/config/config.go
@@ -3,7 +3,14 @@ package config
 import cfg "github.com/filatovw/Wattx-challenge-top-coins/libs/config"
 
 type Config struct {
-	GRPC cfg.GRPC `json:"grpc"`
+	GRPC          cfg.GRPC      `json:"grpc"`
+	CoinMarketCap CoinMarketCap `json:"coinmarketcap"`
+}
+
+type CoinMarketCap struct {
+	Key          string `json:"key"`
+	URL          string `json:"url"`
+	BaseCurrency string `json:"base_currency"`
 }
 
 func (c Config) ServiceName() string {
